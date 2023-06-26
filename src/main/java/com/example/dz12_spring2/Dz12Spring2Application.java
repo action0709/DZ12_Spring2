db87dec91d149068bf2bff28f9ff3cf909f569d6
action0709 <action0709@mail.ru>
2023-06-26 16:20:33 +0400

    1

---
+++ src/main/java/com/example/dz12_spring2/Dz12Spring2Application.java
@@ -0,0 +1,13 @@
+package com.example.dz12_spring2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dz12Spring2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Dz12Spring2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dz12_spring2/Dz12Spring2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dz12_spring2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dz12Spring2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
